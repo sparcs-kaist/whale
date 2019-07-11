dd2286043282f3e6b22da063d2a91db48ceb30db
jwoo0122 <thinkratomos@gmail.com>
2019-07-12 00:16:09 +0900

    Change 'portainer' to 'whale'.

--- app/portainer/components/header-title.js
+++ app/portainer/components/header-title.js
@@ -9,7 +9,7 @@ angular.module('portainer.app')
       scope.username = Authentication.getUserDetails().username;
     },
     transclude: true,
-    template: '<div class="page white-space-normal">{{titleText}}<span class="header_title_content" ng-transclude></span><span class="pull-right user-box" ng-if="username"><i class="fa fa-user-circle" aria-hidden="true"></i> {{username}}</span><a ui-sref="portainer.support" class="pull-right" style="font-size:14px;margin-right:15px;margin-top:2px;"><span class="fa fa-life-ring fa-fw red-icon"></span>  Portainer support</a></div>',
+    template: '<div class="page white-space-normal">{{titleText}}<span class="header_title_content" ng-transclude></span><span class="pull-right user-box" ng-if="username"><i class="fa fa-user-circle" aria-hidden="true"></i> {{username}}</span><a ui-sref="portainer.support" class="pull-right" style="font-size:14px;margin-right:15px;margin-top:2px;"><span class="fa fa-life-ring fa-fw red-icon"></span>  Whale support</a></div>',
     restrict: 'E'
   };
   return directive;
